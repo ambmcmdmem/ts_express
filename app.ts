70e4f3665f94cde1c0ed15d0fbe05ce2c3bea785
yamamoto <ambmcmdmem@au.com>
2022-04-08 15:08:08 +0900

    定数、引数、関数すべてに型宣言を追加

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
-const server = express();
+const server: express.Express = express();
 
 server.listen(3000);
-server.get('/', function (_, response) {
-  response.send('Hello, world');
-});
+server.get(
+  '/',
+  function (_: express.Request, response: express.Response): void {
+    response.send('Hello, world');
+  }
+);

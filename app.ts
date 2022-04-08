a7f5491eb1bc39978f003899ec22b9701dd496a5
yamamoto <ambmcmdmem@au.com>
2022-04-08 16:14:54 +0900

    http://localhost:63519と通信できるように

--- app.ts
+++ app.ts
@@ -3,5 +3,8 @@ const server = express();
 
 server.listen(3000);
 server.get('/', function (_, response): void {
+  response.set({
+    'Access-Control-Allow-Origin': 'http://localhost:63919',
+  });
   response.send('Hello, world');
 });

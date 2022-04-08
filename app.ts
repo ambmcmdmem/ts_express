c2b023528da002184ea96495911fea472f497299
yamamoto <ambmcmdmem@au.com>
2022-04-08 15:22:41 +0900

    不必要な型宣言（定数、引数）の削除

--- app.ts
+++ app.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-const server: express.Express = express();
+const server = express();
 
 server.listen(3000);
-server.get(
-  '/',
-  function (_: express.Request, response: express.Response): void {
-    response.send('Hello, world');
-  }
-);
+server.get('/', function (_, response): void {
+  response.send('Hello, world');
+});
